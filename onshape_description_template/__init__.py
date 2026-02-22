049a4aaacf5e6a878a6768693b91b8e705ab10c2
Justin Lopato <justin@lopato.org>
2026-02-21 20:22:48 -0600

    Add URDF joint names to JOINT_NAMES list
    Added URDF joint names to the JOINT_NAMES list.

--- onshape_description_template/__init__.py
+++ onshape_description_template/__init__.py
@@ -1,3 +1,14 @@
 JOINT_NAMES = [
-	"" # TODO: add all URDF joint names
+	"FL_hip",
+	"FL_a",
+	"FL_l",
+	"FR_hip",
+	"FR_a",
+	"FR_l",
+	"BL_hip",
+	"BL_a",
+	"BL_l",
+	"BR_hip",
+	"BR_a",
+	"BR_l",
 ]
